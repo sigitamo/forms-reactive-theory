678b16ddcde71ab091705f17cc77f2c1ea142c58
sigitamocevice <sigita.z@gmail.com>
2017-08-10 11:48:48 +0300

    validacija ngOnInit metode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -13,10 +13,15 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null),
-      'email': new FormControl (null),
+      'username': new FormControl(null, Validators.required ),
+      'email': new FormControl (null, [Validators.required, Validators.email]),
       'gender': new FormControl('female')
     });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+    this.signupForm.value
   }
 
 }
